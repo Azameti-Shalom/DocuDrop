28534a76bd6c449278d9c04ba84ee0503497215a
Azameti Shalom <shalomazameti10000@gmail.com>
2025-06-07 14:51:06 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function browse(){
+    let fileInput = document.getElementById('fileInput');
+    
+    fileInput.click();
+
+    fileInput.addEventListener('change', function(){
+
+        let file = fileInput.files[0];
+
+        let msg = document.getElementById('msg');
+
+        msg.textContent =`Selected: ${file.name}`;
+
+        msg.style.padding = '10px';
+
+        msg.style.border = '2px dashed green';
+
+    });
+    
+}
